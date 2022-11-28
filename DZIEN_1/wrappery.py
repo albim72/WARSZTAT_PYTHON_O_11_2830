d13158b666141213394cb5bc4820d10cede91631
albim72 <marcin.albiniak72@gmail.com>
2022-11-28 14:13:29 +0100

    Update wrappery.py

--- DZIEN_1/wrappery.py
+++ DZIEN_1/wrappery.py
@@ -9,17 +9,6 @@ def pomiarczasu(funkcja):
         print(f'czas wykonania funkcji: {endtime-starttime} s')
     return wrapper
 
-def sleep(funkcaimport math
-import time
-
-def pomiarczasu(funkcja):
-    def wrapper():
-        starttime = time.perf_counter()
-        funkcja()
-        endtime = time.perf_counter()
-        print(f'czas wykonania funkcji: {endtime-starttime} s')
-    return wrapper
-
 def sleep(funkcja):
     def wrapper():
         time.sleep(4)
@@ -62,30 +51,25 @@ def debug(funkcja):
 def info(i):
     print(f"informacja: {i}")
 
-info("blok nr 324567")ja):
-    def wrapper():
-        time.sleep(4)
-        return funkcja()
+info("blok nr 324567")
+
+def repeater(n):
+    def wrapper(funkcja):
+        def inner(*args):
+            for i in range(n):
+                funkcja(*args)
+        return inner
     return wrapper
 
+@repeater(n=6)
+def komunikat(k,n):
+    print(f'ważny komunikat: {k}, numer: {n}')
+
+komunikat("R5654",7)
 
 
-@pomiarczasu
-@sleep
-def big_lista():
-    sum([i**2 for i in range(10000000) if i%2==0])
+@repeater(n=4)
+def hx(n):
+    print((n-1)**9)
 
-big_lista()
-
-lt = [i**2 for i in range(10000000) if i%2==0]
-@pomiarczasu
-def drugalista():
-    sum(lt)
-
-drugalista()
-
-@pomiarczasu
-def trzecialista():
-    sum([math.pow((i+4)**2,4) for i in range(10000000)])
-
-trzecialista()
+hx(89)
