fde70366b3c3d3436e40eea4c17c4bace5974dbe
albim72 <marcin.albiniak72@gmail.com>
2022-11-28 13:54:39 +0100

    Create wrappery.py

---
+++ DZIEN_1/wrappery.py
@@ -0,0 +1,29 @@
+import math
+import time
+
+def pomiarczasu(funkcja):
+    def wrapper():
+        starttime = time.perf_counter()
+        funkcja()
+        endtime = time.perf_counter()
+        print(f'czas wykonania funkcji: {endtime-starttime} s')
+    return wrapper
+
+@pomiarczasu
+def big_lista():
+    sum([i**2 for i in range(10000000) if i%2==0])
+
+big_lista()
+
+lt = [i**2 for i in range(10000000) if i%2==0]
+@pomiarczasu
+def drugalista():
+    sum(lt)
+
+drugalista()
+
+@pomiarczasu
+def trzecialista():
+    sum([math.pow((i+4)**2,4) for i in range(10000000)])
+
+trzecialista()
