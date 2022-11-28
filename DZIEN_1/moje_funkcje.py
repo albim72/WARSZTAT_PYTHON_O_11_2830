21004a22878c507b878400532b2c9b3836a00d45
albim72 <marcin.albiniak72@gmail.com>
2022-11-28 12:01:27 +0100

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -1,16 +1,22 @@
-import math
+from drugie_funkcje import *
+#funkcja 1
 
-def zewnetrzna():
-    x = "lokalnie"
-    def wewnetrzna():
-        nonlocal x
-        x = "nielokalnie"
-        print(f"wewnętrzne x: {x}")
-    wewnetrzna()
-    print(f"zewnętrzne x: {x}")
-    
-    
-#kolejna funkcja
+def k(j):
+    return j**5
 
-def gx(n,m=4,k=3,b=7):
-    return math.sqrt(n+m)*k-b
+n = 18
+def policz(a,b,c,y):
+    global n
+    n = (a+b)*y - c + n + k(b)
+    return n
+
+print(policz(5,7,2,2))
+print(n)
+
+zewnetrzna()
+
+print(gx(5,8,9,23))
+print(gx(5,8,9))
+print(gx(5))
+
+ranking("Python","C++","Java")
