24a2113fa3a30a4757615aabce8fc77e1866e6e5
albim72 <marcin.albiniak72@gmail.com>
2022-11-28 12:01:53 +0100

    Update drugiefunkcje.py

--- DZIEN_1/drugiefunkcje.py
+++ DZIEN_1/drugiefunkcje.py
@@ -1,3 +1,5 @@
+import math
+
 def zewnetrzna():
     x = "lokalnie"
     def wewnetrzna():
@@ -6,3 +8,20 @@ def zewnetrzna():
         print(f"wewnętrzne x: {x}")
     wewnetrzna()
     print(f"zewnętrzne x: {x}")
+
+
+#kolejna funkcja
+
+def gx(n,m=4,k=3,b=7):
+    return math.sqrt(n+m)*k-b
+
+#W Pythonie nie klasycznego przeciążania fukcji
+# def gx(n,m=4):
+#     return math.sqrt(n+m)
+
+# ranking
+
+def ranking(r1,r2,r3):
+    print(f'ranking języków programowania -> piwerwsze miejsce:{r1},drugie miejsce: {r2},'
+          f'trzecie miejsceL {r3}')
+
