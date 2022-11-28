079c0520ef817f9a411979b65657f77fa717edc9
albim72 <marcin.albiniak72@gmail.com>
2022-11-28 13:29:29 +0100

    Update funkcja_w_funkcji.py

--- DZIEN_1/funkcja_w_funkcji.py
+++ DZIEN_1/funkcja_w_funkcji.py
@@ -11,3 +11,25 @@ def osoba(funkcja,*args):
 
 print(osoba(witaj,"Leon"))
 print(osoba(konkurs,"Olga",67))
+
+#przykład 2
+
+def rejestracja(oplata):
+    def lista():
+        return "jesteś na liście zawodników"
+    def brak():
+        return "jeśli chcesz znależć się na liście zawodników, dokonaja wpłaty"
+    def error():
+        return "podaj 1 - wpłata, 0 - brak wpłaty"
+
+    if oplata == 1:
+        return lista
+    elif oplata == 0:
+        return brak
+    else:
+        return error
+
+
+print(rejestracja(1)())
+print(rejestracja(0)())
+print(rejestracja(13)())
