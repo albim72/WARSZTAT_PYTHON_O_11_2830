b3e03e4ac63f6a104520414df9ba03837737ad52
albim72 <marcin.albiniak72@gmail.com>
2022-11-28 13:22:52 +0100

    Create funkcja_w_funkcji.py

---
+++ DZIEN_1/funkcja_w_funkcji.py
@@ -0,0 +1,13 @@
+#przykład 1
+
+def witaj(imie):
+    return f"Miło Cię widzieć {imie}"
+
+def konkurs(imie,punkty):
+    return f"uczestnik {imie}, liczba punktów = {punkty}"
+
+def osoba(funkcja,*args):
+    return funkcja(*args)
+
+print(osoba(witaj,"Leon"))
+print(osoba(konkurs,"Olga",67))
