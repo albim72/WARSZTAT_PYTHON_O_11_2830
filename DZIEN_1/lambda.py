5d6cd7226f59d8e9c878bbb6f16334d3d2b239b3
albim72 <marcin.albiniak72@gmail.com>
2022-11-28 12:57:58 +0100

    Update lambda.py

--- DZIEN_1/lambda.py
+++ DZIEN_1/lambda.py
@@ -13,5 +13,18 @@ def ob(k):
 
 print(ob(2)(n=6, a=2))
 
-
 num = [67,2,5,177,-99,0,9,12,7,1,-4,0,5,7,18]
+
+#stwórz listę liczbyparz i przekaż do niej wszystkie wartości parzyste z listy num
+#użyj funkcji filter, która jest funcją standardową wyższego rzędu(parametr zerowy funkcji jest funckją,
+# paramietr pierwszy jest kolekcją danych
+
+liczbyparz = list(filter(lambda x:x%2==0,num))
+print(liczbyparz)
+
+def warunek(x):
+    return x%2==0
+
+liczbyparz_2 = list(filter(warunek,num))
+print(liczbyparz_2)
+
