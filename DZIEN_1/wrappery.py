d09089330732d0f38f24ef138bd2700bac179481
albim72 <marcin.albiniak72@gmail.com>
2022-11-28 13:59:49 +0100

    Update wrappery.py

--- DZIEN_1/wrappery.py
+++ DZIEN_1/wrappery.py
@@ -9,7 +9,16 @@ def pomiarczasu(funkcja):
         print(f'czas wykonania funkcji: {endtime-starttime} s')
     return wrapper
 
+def sleep(funkcja):
+    def wrapper():
+        time.sleep(4)
+        return funkcja()
+    return wrapper
+
+
+
 @pomiarczasu
+@sleep
 def big_lista():
     sum([i**2 for i in range(10000000) if i%2==0])
 
