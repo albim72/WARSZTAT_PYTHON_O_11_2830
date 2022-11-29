707dd0eb4a4cf6cddc5a51fa979b5875b71eea84
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 15:12:04 +0100

    Update filemanager.py

--- DZIEN_2/filemanager.py
+++ DZIEN_2/filemanager.py
@@ -15,4 +15,15 @@ class FileManager:
 
 
 
-        
+#utworzenie nowgo pliku
+with FileManager('test.txt','w','utf-8') as f:
+    f.write('to jest pierwsza linia wiadomości\n')
+
+#nadpisywanie nowgo pliku
+with FileManager('test.txt','a','utf-8') as f:
+    f.write('to jest kolejna linia wiadomości\n')
+
+#wyświetlenie zawartości pliku
+with FileManager('test.txt','r','utf-8') as f:
+    print(f.read())
+
