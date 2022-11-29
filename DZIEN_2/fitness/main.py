c913f95f62f5527744a462120617e31871069238
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 10:09:22 +0100

    Update main.py

--- DZIEN_2/fitness/main.py
+++ DZIEN_2/fitness/main.py
@@ -1,5 +1,7 @@
 from rep_trener.osoba import Osoba
 from rep_trener.sport import Sport
+from rep_trener.klub import  Klub
+
 
 
 print("___________________ Osoba ______________________")
@@ -11,3 +13,7 @@ print(f'czy osoba jest trenerem? ({os1.czytrener()})')
 print("___________________ Sport ______________________")
 sp1 = Sport("Biegi Ultra",7,"50km 7h 23min 34s")
 print(sp1.infosport())
+
+print("___________________ Klub ______________________")
+kb1 = Klub(12,"Herkules","ul. Bursztynowa 45, Krosno")
+print(kb1.infoklub())
