d6cea6148564b8c1293762e824580bca129315ac
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 11:01:45 +0100

    Update mian.py

--- DZIEN_2/resistor/mian.py
+++ DZIEN_2/resistor/mian.py
@@ -1,4 +1,5 @@
 from oldresistor import OldResistor
+from resistor import Resistor
 
 print("________________klasa OldResistor____________________")
 r0 = OldResistor(10.22E2)
@@ -6,3 +7,8 @@ print(r0)
 print(r0._ohms)
 r0.set_ohms(2.88E3)
 print(r0.get_ohms())
+print("________________klasa Resistor____________________")
+r1 = Resistor(50E3)
+r1.ohms = 10E3
+print(f'układ elektryczny:\noporność: {r1.ohms} om\nnapięcie prądu:'
+      f' {r1.voltage} V\nnatężenie prądu: {r1.current} A')
