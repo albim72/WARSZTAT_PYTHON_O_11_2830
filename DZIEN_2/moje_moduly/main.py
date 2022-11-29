ee7cbaf32608eef4f8d95767fb9b623c60090ce8
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 09:33:22 +0100

    Update main.py

--- DZIEN_2/moje_moduly/main.py
+++ DZIEN_2/moje_moduly/main.py
@@ -2,6 +2,7 @@
 #import dane as dn
 from dane import nr_filia,book,WSPOLCZYNNIK_RABATOWY
 from rep_funkcje.bfunkcje import czytaj_dict,czytaj_liste
+from rep_klasy.czytajdane import CDane
 
 print(nr_filia)
 print(book)
@@ -10,3 +11,7 @@ print(f"podstawowy rabat: {book['cena']*WSPOLCZYNNIK_RABATOWY} zł")
 print("__________________ czytanie za pomocą funkcji ________________")
 czytaj_liste(nr_filia)
 czytaj_dict(book)
+print("__________________ czytanie za pomocą klasy i jej metod ________________")
+cd = CDane(nr_filia,book)
+cd.czytaj_l()
+cd.czytaj_s()
