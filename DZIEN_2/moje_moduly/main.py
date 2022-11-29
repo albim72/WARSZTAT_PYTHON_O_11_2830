efae171d92c3762cc7def70db09aa416f05c2863
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 09:26:28 +0100

    Update main.py

--- DZIEN_2/moje_moduly/main.py
+++ DZIEN_2/moje_moduly/main.py
@@ -1,7 +1,12 @@
 #import dane
 #import dane as dn
 from dane import nr_filia,book,WSPOLCZYNNIK_RABATOWY
+from rep_funkcje.bfunkcje import czytaj_dict,czytaj_liste
 
 print(nr_filia)
 print(book)
 print(f"podstawowy rabat: {book['cena']*WSPOLCZYNNIK_RABATOWY} zł")
+
+print("__________________ czytanie za pomocą funkcji ________________")
+czytaj_liste(nr_filia)
+czytaj_dict(book)
