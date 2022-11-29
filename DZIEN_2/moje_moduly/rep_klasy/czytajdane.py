2a6d29d0d0843a1b847b34b79903c1b383606d5e
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 09:31:10 +0100

    Create czytajdane.py

---
+++ DZIEN_2/moje_moduly/rep_klasy/czytajdane.py
@@ -0,0 +1,12 @@
+from rep_funkcje.bfunkcje import *
+
+class CDane:
+    def __init__(self,lista,slownik):
+        self.lista = lista
+        self.slownik = slownik
+        
+    def czytaj_l(self):
+        return czytaj_liste(self.lista)
+    
+    def czytaj_s(self):
+        return czytaj_dict(self.slownik)
