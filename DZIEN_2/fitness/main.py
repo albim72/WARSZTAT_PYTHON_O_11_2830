ed2a43901752706ebca8f18a2d0125fb0e6265f5
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 10:28:03 +0100

    Update main.py

--- DZIEN_2/fitness/main.py
+++ DZIEN_2/fitness/main.py
@@ -1,6 +1,7 @@
 from rep_trener.osoba import Osoba
 from rep_trener.sport import Sport
 from rep_trener.klub import  Klub
+from trener import Trener
 
 
 
@@ -17,3 +18,11 @@ print(sp1.infosport())
 print("___________________ Klub ______________________")
 kb1 = Klub(12,"Herkules","ul. Bursztynowa 45, Krosno")
 print(kb1.infoklub())
+
+print("___________________ Trener ______________________")
+tr1 = Trener("Jan","Kot",45,78,180,3453454,150,"Trójbój",10,450)
+tr1.print_osoba()
+print(tr1.infosport())
+print(tr1.print_trener())
+print("___________________ Trener ______________________")
+tr1 = Trener("Olga","Knot",28,61,176,6546,130,"Biegi Ultra",8,"102km 18h 34min 6s",4,"Lotos","ul Trojańska 3 Ostrów Wlk")
