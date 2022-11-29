7fce4eb77cb126bc1706bc8d7f5ad65c0f7ed57d
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 11:19:20 +0100

    Update mian.py

--- DZIEN_2/resistor/mian.py
+++ DZIEN_2/resistor/mian.py
@@ -1,5 +1,6 @@
 from oldresistor import OldResistor
 from resistor import Resistor
+from voltage import VoltageResistance
 
 print("________________klasa OldResistor____________________")
 r0 = OldResistor(10.22E2)
@@ -12,3 +13,11 @@ r1 = Resistor(50E3)
 r1.ohms = 10E3
 print(f'układ elektryczny:\noporność: {r1.ohms} om\nnapięcie prądu:'
       f' {r1.voltage} V\nnatężenie prądu: {r1.current} A')
+
+print("________________klasa VoltageResistance____________________")
+r2 = VoltageResistance(1E3)
+print(f'natężenie początkow prądu: {r2.current} A')
+r2.voltage = 35
+print(f'natężenie końcowe prądu: {r2.current} A')
+print(f'napięcie końcowe prądu: {r2.voltage} V')
+
