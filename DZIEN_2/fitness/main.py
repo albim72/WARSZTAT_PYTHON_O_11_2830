83cc14cdf247134986f1c53278a11fed3c7fd08c
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 10:01:12 +0100

    Update main.py

--- DZIEN_2/fitness/main.py
+++ DZIEN_2/fitness/main.py
@@ -1,5 +1,13 @@
 from rep_trener.osoba import Osoba
+from rep_trener.sport import Sport
 
+
+print("___________________ Osoba ______________________")
 os1 = Osoba("Feliks","Kot",31,74,176)
 os1.print_osoba()
+
 print(f'czy osoba jest trenerem? ({os1.czytrener()})')
+
+print("___________________ Sport ______________________")
+sp1 = Sport("Biegi Ultra",7,"50km 7h 23min 34s")
+print(sp1.infosport())
