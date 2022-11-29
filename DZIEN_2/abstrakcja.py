36b2446159ace67992b52bc159a822063f8236a8
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 13:19:47 +0100

    Update abstrakcja.py

--- DZIEN_2/abstrakcja.py
+++ DZIEN_2/abstrakcja.py
@@ -22,6 +22,27 @@ class Abstract(ABC):
         return self.x+self.y
 
 
+class MojaKlasa(Abstract):
+    def __init__(self,x,y,z):
+        super().__init__(x,y)
+        self.z=z
+
+    def fxy(self):
+        return self.x+self.y+self.z
+
+    def gxy(self):
+        return super().gxy()+(self.z-7)
+
+
+m = MojaKlasa(9,3,2)
+
+print(f'funkcja fxy -> {m.fxy()}')
+print(f'funkcja gxy -> {m.gxy()}')
+
+print(f'pierwotne x: {m.xp}')
+print(f'pierwotne y: {m.yp}')
+
+
 
 
 
