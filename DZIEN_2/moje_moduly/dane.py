26af1a64a1783f4fd53a3f3d2bc63457f5e06046
albim72 <marcin.albiniak72@gmail.com>
2022-11-29 09:14:20 +0100

    Create dane.py

---
+++ DZIEN_2/moje_moduly/dane.py
@@ -0,0 +1,11 @@
+nr_filia = [2,7,9,11,23,144,59,89]
+
+book = {
+    "tytuł":"Wiedźmin",
+    "autor":"Andrzej Sapkowski",
+    "cena":43,
+    "liczba_stron":312,
+    "nr_filii":nr_filia
+}
+
+WSPOLCZYNNIK_RABATOWY:float = 0.12
