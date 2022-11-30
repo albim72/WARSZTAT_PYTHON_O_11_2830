6e374bc390d1475a21d335ac6babd0a3eb7c9ca8
albim72 <marcin.albiniak72@gmail.com>
2022-11-30 11:18:57 +0100

    Update readxml.py

--- DZIEN_3/readxml.py
+++ DZIEN_3/readxml.py
@@ -5,6 +5,8 @@ try:
     root = tree.getroot()
     for child in root:
         print(f"element: {child.tag}, atrybut: {child.attrib}")
-
+    print(root[0])
+    print(root[0][2])
+    print(root[0][2].text)
 except:
     print("element nie istnieje!")
