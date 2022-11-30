cceed4f956d1a1eeac040d6ded5110239375b499
albim72 <marcin.albiniak72@gmail.com>
2022-11-30 10:00:53 +0100

    Update samochod_cls.py

--- DZIEN_3/samochod_cls.py
+++ DZIEN_3/samochod_cls.py
@@ -5,11 +5,20 @@ class Samochod:
         self.rocznik = rocznik
         self.cena = cena
         self.rata = rata
-        
+
     @staticmethod
     def salon(miasto):
         return f"salon sprzedaży -> miasto {miasto}"
-    
+
     @classmethod
     def obliczenieRaty(cls,cena,miesiac):
         return cls("Opel","Insignia",2019,56000,1.3*cena/miesiac)
+
+sm = Samochod("Jeep","Cherokee",2017,124500,2700)
+sm2 = Samochod.obliczenieRaty(57800,48)
+
+print(sm.salon("Toruń"))
+print(sm.rata)
+
+print(sm2.salon("Kraków"))
+print(f"{sm2.rata:.2f}")
