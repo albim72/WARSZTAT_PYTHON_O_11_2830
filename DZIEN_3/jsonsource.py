1c2069579d655703b652b2c15fa768238c7d4df0
albim72 <marcin.albiniak72@gmail.com>
2022-11-30 10:56:06 +0100

    Update jsonsource.py

--- DZIEN_3/jsonsource.py
+++ DZIEN_3/jsonsource.py
@@ -1,3 +1,41 @@
 import json
 
 jsonbook = '{"tytul":"Hobbit","autor":"J.R.R. Tolkien","oprawa":"twarda","lstron":288,"cena":32.66}'
+
+print(jsonbook)
+print(type(jsonbook))
+
+book = json.loads(jsonbook)
+print(book)
+print(type(book))
+print(f"tytul: {book['tytul']}, cena: {book['cena']} zł")
+
+kolor = {
+    "id":2354365,
+    "nazwa_koloru":"żółty ciemny",
+    "paleta":"UHK56333",
+    "lodcieni":12
+}
+
+jsonkolor = json.dumps(kolor, indent=4)
+print(jsonkolor)
+
+
+with open("kolor.json","w",encoding="utf-8") as f:
+    f.write(jsonkolor)
+
+with open("kolor.json","r",encoding="utf-8") as f:
+    kolor_dict = json.load(f)
+
+print(kolor_dict)
+
+print("*********************************************")
+
+infofund = '{"organizacja":"Fundacja Biotech","miasto":"Krosno","kraj":"Polska"}'
+
+ekstra = {"id_proj":4345345,"zakres":"algorytmy AI"}
+
+z = json.loads(infofund)
+z.update(ekstra)
+info_new = json.dumps(z)
+print(info_new)
