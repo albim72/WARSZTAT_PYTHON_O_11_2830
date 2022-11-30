c3fdf1f7b36a0324fa0981e47fd080fd5f3aa5b6
albim72 <marcin.albiniak72@gmail.com>
2022-11-30 12:42:56 +0100

    Update saxparser.py

--- DZIEN_3/saxparser.py
+++ DZIEN_3/saxparser.py
@@ -9,8 +9,36 @@ class UchwytFilmu(xml.sax.ContentHandler):
         self.kraj = ""
         self.czas_t = ""
         self.gatunek = ""
-        
+
     def startElement(self,tag,attrs):
         self.CurrentData = tag
         if tag == "film":
             print("_______________ film ___________________")
+
+    def endElement(self,tag):
+        if self.CurrentData == "id_filmu":
+            print(f"identyfikator filmu: {self.id}")
+        elif self.CurrentData == "tytul":
+            print(f"tytuł filmu: {self.tytul}")
+        elif self.CurrentData == "rok":
+            print(f"rok produkcji filmu: {self.rok}")
+        elif self.CurrentData == "kraj":
+            print(f"kraj produkcji filmu: {self.kraj}")
+        elif self.CurrentData == "czas_trwania":
+            print(f"czas trwania filmu: {self.czas_t}")
+        elif self.CurrentData == "gatunek":
+            print(f"gatunek filmu: {self.gatunek}")
+
+    def characters(self,content):
+        if self.CurrentData == "id_filmu":
+            self.id = content
+        elif self.CurrentData == "tytul":
+            self.tytul = content
+        elif self.CurrentData == "rok":
+            self.rok = content
+        elif self.CurrentData == "kraj":
+            self.kraj = content
+        elif self.CurrentData == "czas_trwania":
+            self.czas_t = content
+        elif self.CurrentData == "gatunek":
+            self.gatunek = content
